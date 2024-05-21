c5da9207de66f128e488b757d6ba7d9e33acca63
alsaady20 <alsaady23@gmail.com>
2024-05-20 22:29:32 -0400

    correct commit

--- src/eclipseSoloProjA/HelloWorld.java
+++ src/eclipseSoloProjA/HelloWorld.java
@@ -4,6 +4,6 @@ public class HelloWorld {
 	
 	public static void main(String[] args) {
 		System.out.println("Hello world");
-		System.out.println("Next alternate Line");
+		System.out.println("Next alternate Line correct");
 	}
 }
